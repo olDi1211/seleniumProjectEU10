0ecdc08529acb723bfc54d1495208ade7c7aa010
Dmytro Oleksiiuk <dmytrooleksiiuk@gmail.com>
2023-01-18 00:02:01 +0100

    Day7 finished

--- src/test/java/com/cydeo/tests/day7_webtabies_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtabies_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,6 +1,7 @@
 package com.cydeo.tests.day7_webtabies_utilities_javafaker;
 
 import com.cydeo.utilities.BrowserUtils;
+import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -52,6 +53,38 @@ public class T3_CRM_LOGIN {
 
 
 
+
+    }
+
+    @Test
+    public void crm_login_test_2(){
+
+
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.navigate().to("https://login1.nextbasecrm.com/");
+
+        //Calling my utility method to login helpdesk1
+        CRM_Utilities.crm_login(driver);
+
+        //6. Verify title is as expected:
+        //Expected: My tasks
+        BrowserUtils.verifyTitle(driver,"My tasks");
+
+    }
+
+    @Test
+    public void crm_login_test_3(){
+
+
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.navigate().to("https://login1.nextbasecrm.com/");
+
+        //Calling my utility method to login helpdesk1
+        CRM_Utilities.crm_login(driver,"helpdesk2@cybertekschool.com", "UserUser");
+
+        //6. Verify title is as expected:
+        //Expected: My tasks
+        BrowserUtils.verifyTitle(driver,"Portal");
 
     }
 
